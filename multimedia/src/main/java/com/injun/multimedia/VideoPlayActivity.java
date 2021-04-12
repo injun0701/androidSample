5bafcb1b63b41454141f89216b1bfe35f598138b
inter <injun0701>
2021-04-12 18:24:47 +0900

    BottomNavigationView 샘플, 음악 플레이어, 비디오 플레이어 샘플 구현

---
+++ multimedia/src/main/java/com/injun/multimedia/VideoPlayActivity.java
@@ -0,0 +1,66 @@
+package com.injun.multimedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.VideoView;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class VideoPlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_play);
+
+        //VideoView 가져오기
+        VideoView videoView = (VideoView)findViewById(R.id.videoView);
+        //재생할 비디오 설정
+        MediaController mc = new MediaController(this);
+        videoView.setMediaController(mc);
+
+        //비디오 파일 경로
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.trailer;
+        videoView.setVideoURI(Uri.parse(path));
+        videoView.requestFocus();
+
+        //재생 버튼 이벤트 처리
+        Button startBtn = (Button)findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoView.seekTo(0);
+                videoView.start();
+            }
+        });
+
+        //비디오 재생 준비가 되면
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                Snackbar.make(getWindow().getDecorView().getRootView(), "재생 준비 완료", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        
+        //볼륨을 최대로 설정
+        Button volumeBtn = (Button)findViewById(R.id.volumeBtn);
+        volumeBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                AudioManager mAudioManager = (AudioManager)
+                        getSystemService(AUDIO_SERVICE);
+                int maxVolume =
+                        mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume,
+                        AudioManager.FLAG_SHOW_UI);
+            }
+        });
+    }
+}
---
+++ supportlibrary/src/main/java/com/injun/supportlibrary/Fragment1.java
@@ -0,0 +1,20 @@
+package com.injun.supportlibrary;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+    //화면을 만들어주는 메서드
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+}
